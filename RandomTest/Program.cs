b70d5bc20281a839930b51737523732ceda6b1fe
JNeto <joaomendes15@gmail.com>
2023-03-31 02:02:02 +0100

    Random Test finished, ex 6 done

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,13 +6,18 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r1 = new Random(1233);
+            Random r2 = new Random(212);
+            
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
+           
             Console.WriteLine();
+            
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r2.Next());
+
+           
         }
     }
 }
