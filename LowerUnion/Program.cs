323f6196d09daa0db933492622a8f478a64b5c93
JNeto <joaomendes15@gmail.com>
2023-03-31 13:53:23 +0100

    almost done, to lower done

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LowerUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            List<String> strs = new List<string>();
+            args.ToList().ForEach(s => strs.Add(s.ToLower()));
+            strs.Sort();
+            strs.ForEach(str => Console.Write(str + " "));
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
