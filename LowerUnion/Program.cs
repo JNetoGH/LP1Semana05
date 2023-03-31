a70843682bf8d60a428052a484f0e84f734b9400
JNeto <joaomendes15@gmail.com>
2023-03-31 13:57:42 +0100

    ex 9 done

--- LowerUnion/Program.cs
+++ LowerUnion/Program.cs
@@ -9,17 +9,22 @@ namespace LowerUnion
     {
         static void Main(string[] args)
         {
-
             
             List<String> strs = new List<string>();
             args.ToList().ForEach(s => strs.Add(s.ToLower()));
             strs.Sort();
             strs.ForEach(str => Console.Write(str + " "));
-            Console.WriteLine();
 
+            StringBuilder finalString = new StringBuilder();
+            for (int i = 0; i < strs.Count; i++)
+            {
+                finalString.Append(strs[i]);
+                if (i == strs.Count -1)
+                    continue;
+                finalString.Append("_");
+            }
 
-
-
+            Console.WriteLine(finalString);
 
         }
     }
