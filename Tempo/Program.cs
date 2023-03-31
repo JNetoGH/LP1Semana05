7519e99cac80a465c06219f4afd99f32665b7674
JNeto <joaomendes15@gmail.com>
2023-03-31 01:39:04 +0100

    ex 4 done, Tempo finished

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
-namespace Tempo {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+namespace Tempo 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(300);
+
+            Console.WriteLine($"{crono1.Elapsed.TotalSeconds:F2}");
+            Console.WriteLine($"{crono2.Elapsed.TotalSeconds:F2}");
+            
         }
     }
 }
